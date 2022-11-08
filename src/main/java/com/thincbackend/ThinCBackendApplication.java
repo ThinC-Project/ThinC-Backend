2aef05743c2fc7b1f85ac5041a5c1ea3fc18b455
jaehyeon2 <wol5971@gmail.com>
2022-11-08 20:40:31 +0900

    create project (11.08)

---
+++ src/main/java/com/thincbackend/ThinCBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thincbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinCBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinCBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thincbackend/ThinCBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thincbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinCBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
