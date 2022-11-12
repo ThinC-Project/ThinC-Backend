ccddb0fb6b43c951c0189b371071b99e32400648
jaehyeon2 <wol5971@gmail.com>
2022-11-12 13:09:38 +0900

    add mypage function (11.12)

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping({"/", "/home", "/main"})
@@ -31,6 +34,15 @@ public class MainController {
     @GetMapping("/shopping")
     public String shoppingPage(){
         return "shopping";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage(HttpSession session, Model model){
+        String Nickname = session.getAttribute("Nickname").toString();
+        String UserID = session.getAttribute("UserID").toString();
+        model.addAttribute("Nickname", Nickname);
+        model.addAttribute("UserID", UserID);
+        return "mypage";
     }
 
 }
