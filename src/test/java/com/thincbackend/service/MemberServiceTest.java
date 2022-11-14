2cf2f217767b7ec033a316927924b61d890a19d4
jaehyeon2 <wol5971@gmail.com>
2022-11-14 22:47:35 +0900

    Member Service Test (11.14)

--- src/test/java/com/thincbackend/service/MemberServiceTest.java
+++ src/test/java/com/thincbackend/service/MemberServiceTest.java
@@ -1,4 +1,4 @@
-package com.thincbackend.repository;
+package com.thincbackend.service;
 
 import com.thincbackend.domain.Member;
 
@@ -9,13 +9,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@Transactional
 @TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
 class MemberServiceTest {
-
 
     @Autowired
     private MemberService memberService;
@@ -32,13 +35,26 @@ class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("유저 등록 테스트")
+    @DisplayName("유저_등록_테스트")
     public void saveMemberTest(){
         Member member = createMember();
         Member savedMember = memberService.saveMember(member);
 
-        assertEquals(member.getNickname(), savedMember.getNickname());
+        assertThat(savedMember.getNickname()).isEqualTo("nickname");
     }
 
+
+    @Test
+    @DisplayName("유저_아이디_중복_확인_테스트")
+    public void validDuplicationTest(){
+        Member member1 = createMember();
+        memberService.saveMember(member1);
+
+        Member member2 = createMember();
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.saveMember(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 가입된 아이디입니다.");
+
+    }
 
 }
