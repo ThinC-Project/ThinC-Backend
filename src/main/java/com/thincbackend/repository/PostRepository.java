0c1aca55ba3766d0ac496abca51d849101057171
jaehyeon2 <wol5971@gmail.com>
2022-11-16 11:07:09 +0900

    Repository query reconstruction (11.16)

--- src/main/java/com/thincbackend/repository/PostRepository.java
+++ src/main/java/com/thincbackend/repository/PostRepository.java
@@ -3,22 +3,19 @@ package com.thincbackend.repository;
 import com.thincbackend.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = "select * from Post where id = #{id}", nativeQuery = true)
-    Post findPostById(Long id);
+    @Query(value = "select * from Post where title like %:keyword% OR content like %:keyword%", nativeQuery = true)
+    List<Post> findPostByKeyword(@Param("keyword") String keyword);
 
-    List<Post> findAll();
-
-    @Query(value = "select * from Post where title like %#{keyword}% OR content like %#{keyword}%", nativeQuery = true)
-    List<Post> findPostByKeyword(String keyword);
-
-    @Query(value = "select * from Post where owner = #{nickname}", nativeQuery = true)
+    @Query(value = "select * from Post where owner = :nickname", nativeQuery = true)
     List<Post> findOwnerPost(String nickname);
 
 }
--- src/main/java/com/thincbackend/repository/RecipeRepository.java
+++ src/main/java/com/thincbackend/repository/RecipeRepository.java
@@ -10,23 +10,14 @@ import java.util.List;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long>{
 
+    @Query(value = "select * from Recipe where title like %:keyword% OR intigrate like %:keyword% OR process like %:keyword%", nativeQuery = true)
+    List<Recipe> findByKeyword(String keyword);
 
-    @Query(value = "select * from Recipe where id = id", nativeQuery = true)
-    Recipe findRecipeById(Long id);
+    List<Recipe> findByCategory(String category);
 
-    @Query(value = "select * from Recipe", nativeQuery = true)
-    List<Recipe> findAllRecipe();
+    List<Recipe> findByOwner(String nickname);
 
-    @Query(value = "select * from Recipe where title like %#{keyword}% OR intigrate like %#{keyword}% OR process like %#{keyword}%", nativeQuery = true)
-    List<Recipe> findRecipeByKeyword(String keyword);
-
-    @Query(value = "select * from Recipe where category = #{category}", nativeQuery = true)
-    List<Recipe> findRecipeByCategory(String category);
-
-    @Query(value = "select * from Recipe where owner = #{nickname}", nativeQuery = true)
-    List<Recipe> findOwnerRecipe(String nickname);
-
-    @Query(value = "select * from Recipe where owner != #{nickname}", nativeQuery = true)
+    @Query(value = "select * from Recipe where owner != :nickname", nativeQuery = true)
     List<Recipe> findOtherRecipe(String nickname);
 
 }
