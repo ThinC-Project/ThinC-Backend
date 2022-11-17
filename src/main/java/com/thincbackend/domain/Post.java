b4f5f52d405f5be0cb12833a5f8b2d67aeaf308c
jaehyeon2 <wol5971@gmail.com>
2022-11-17 15:17:56 +0900

    add Post.java @Setter annotation (11.17)

--- src/main/java/com/thincbackend/domain/Post.java
+++ src/main/java/com/thincbackend/domain/Post.java
@@ -4,12 +4,14 @@ import com.thincbackend.dto.PostFormDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 
 @NoArgsConstructor
 @Getter
+@Setter
 @Table(name = "posts")
 @Entity
 public class Post {
