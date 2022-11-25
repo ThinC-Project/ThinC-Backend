c95a8be2227bf4d50938c36879375feb935a1695
jaehyeon2 <wol5971@gmail.com>
2022-11-26 00:51:41 +0900

    ConnectController(join, login) connection (11.25)

--- src/main/java/com/thincbackend/controller/ConnectController.java
+++ src/main/java/com/thincbackend/controller/ConnectController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,42 +15,45 @@ import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
+@RestController
 @RequestMapping("/")
 public class ConnectController {
 
     private final MemberService memberService;
 
     @GetMapping("/join")
-    public String joinForm(Model model){
-        model.addAttribute("memberFormDto", new MemberFormDto());
-
-        return "join";
-    }
-    @PostMapping("/join")
-    public String joinForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
-            return "/join";
-        }
+    public String joinForm(@Valid MemberFormDto memberFormDto, HttpServletRequest request){
         try{
             Member member = Member.createMember(memberFormDto);
             Member savedMember = memberService.saveMember(member);
             System.out.println("Member ID : " + savedMember.getMemberID());
+            return "join success";
         } catch(IllegalStateException e){
-            model.addAttribute("errorMessage", e.getMessage());
-            return "/join";
+            return "error";
         }
-        return "redirect:/login";
     }
+//    @PostMapping("/join")
+//    public String joinForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+//        if(bindingResult.hasErrors()){
+//            return "/join";
+//        }
+//        try{
+//            Member member = Member.createMember(memberFormDto);
+//            Member savedMember = memberService.saveMember(member);
+//            System.out.println("Member ID : " + savedMember.getMemberID());
+//        } catch(IllegalStateException e){
+//            model.addAttribute("errorMessage", e.getMessage());
+//            return "/join";
+//        }
+//        return "redirect:/login";
+//    }
 
     @GetMapping("/login")
-    public String loginForm(){
-        return "/login";
-    }
-    @PostMapping("/login")
-    public String loginForm(HttpSession session, HttpServletRequest httpServletRequest, Model model){
+    public String loginForm(HttpSession session, HttpServletRequest request){
         try{
-            String MemberID = httpServletRequest.getParameter("ID");
-            String MemberPW = httpServletRequest.getParameter("PW");
+            String MemberPW = request.getParameter("pw");
+            String MemberID = request.getParameter("id");
+            System.out.println(MemberID+" "+MemberPW);
 
             Member member = memberService.findByMemberId(MemberID);
             if (member!=null && member.getPassword()==MemberPW){
@@ -60,15 +61,14 @@ public class ConnectController {
 //                session.setAttribute("MemberID", member.getMemberID());
                 memberService.createMemberSession(member, session);
                 System.out.println("login Success!");
-                return "redirect:/";
+                return "login success";
+            }else{
+                System.out.println("no member");
+
+                return "no member";
             }
         } catch(IllegalStateException e){
-            model.addAttribute("errorMessage", e.getMessage());
-            return "/login";
+            return "error";
         }
-
-        return "/login";
     }
-
-
 }
