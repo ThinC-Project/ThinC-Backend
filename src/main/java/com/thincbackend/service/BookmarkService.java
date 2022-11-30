fb3879e37d48189bd6c6043d88d75c78db177532
jaehyeon2 <wol5971@gmail.com>
2022-11-30 16:37:57 +0900

    bookmark service add annotation (11.30)

--- src/main/java/com/thincbackend/service/BookmarkService.java
+++ src/main/java/com/thincbackend/service/BookmarkService.java
@@ -2,9 +2,15 @@ package com.thincbackend.service;
 
 import com.thincbackend.domain.Bookmark;
 import com.thincbackend.repository.BookmarkRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class BookmarkService {
     private BookmarkRepository bookmarkRepository;
 
